c39d52c0ee142ac88a0edae5bff1dee8aee38de6
Joxxx69 <santiago.gualotuna@hotmail.com>
2023-07-02 21:51:20 -0500

    cambios

--- login-signup/helpers/login.js
+++ login-signup/helpers/login.js
@@ -1,23 +1,8 @@
-let productos;
-if (localStorage.length>0) {
-     productos = JSON.parse(localStorage.getItem('prod'));;
-} else {
-    productos = [];
-}
 
-document.querySelector('.form__productos').addEventListener('submit', e => {
+
+document.querySelector('#form__signIn').addEventListener('submit', e => {
     e.preventDefault();
-    
-    let id = document.querySelector('#idProducto')
-    let desc = document.querySelector('#descProducto')
-    let nombre = document.querySelector('#nameProducto')
-    let img = document.querySelector('#imgProducto')
-    let precio = document.querySelector('#precioProducto')
-    const producto = { id:id.value,name:nombre.value, desc:desc.value, img:img.value, precio:precio.value };
-    productos.push(producto)
-    localStorage.setItem('prod', JSON.stringify(productos));
-    id.textContent = "";
-    desc.textContent = "";
-    img.textContent = "";
-    precio.textContent = 0;
+    const data = Object.fromEntries(new FormData(e.target));
+    localStorage.setItem('user', JSON.stringify(data));
+    window.location.href = './principal.html'
 })
