36ff95aa71909b01a0617982cf03f047a033f6ca
Joxxx69 <santiago.gualotuna@hotmail.com>
2023-07-02 21:53:15 -0500

    cambios

--- login-signup/helpers/principal.js
+++ login-signup/helpers/principal.js
@@ -1,7 +1,7 @@
 const user = JSON.parse(localStorage.getItem('user'));
 console.log(user);
 const userItem = document.querySelector('#user__item');
-userItem.textContent = user.name;
+userItem.textContent = user.name || user.email;
 console.log(userItem)
 
 
